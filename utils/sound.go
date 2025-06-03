4614742663264eabaf93664cdacc20caa0d2ed30
jungju <leejungju.go@gmail.com>
2025-06-03 17:33:24 +0900

    fix

--- utils/sound.go
+++ utils/sound.go
@@ -11,7 +11,7 @@ import (
 )
 
 func main() {
-	r := "../static/assets/sentence/ivealwayswanted"
+	r := "../static/assets/sentence/itshardformeto"
 	d := filepath.Join(r, "audio")
 	os.MkdirAll(d, 0755)
 
